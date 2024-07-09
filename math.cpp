ea7882dfa708b5493678ae375269d576b1d85a66
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2024-07-09 13:41:51 +0300

    Update math.cpp

--- math.cpp
+++ math.cpp
@@ -1,5 +1,5 @@
-         تتابعه حسابيه
-      a1 a2 a3 ------ an   
+         متتابعه حسابيه
+      a1 a1+d a1+2d ------ an   
 int sum (int ai,int an,int n) {
   return ((ai + an) * n) / 2;
 }
@@ -9,3 +9,16 @@ int find_an(int a1,int n,int d){
 int find_d(int a1,int a2){
   return a2-a1;
 }
+         متتابعه هندسيه 
+      a1 a1*r a1*r*r ------ an   
+int sum (int ai,int n) {
+  return a1*(1-pow(r,n))/(1-r);
+}
+int find_an(int a1,int n,int d){
+  return a1 * pow(r,(n-1));
+}
+int find_r(int a1,int a2){
+  return a2/a1;
+}
+sum all of numbers to n = n*(n+1)/2
+sum all of even numders to n = (n/2)*((n/2)+1)/2
