4b78231e891f335710ac72f4bf8318ebf9a416a2
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2024-07-17 20:21:33 +0300

    Update NumberTheory.cpp

--- NumberTheory.cpp
+++ NumberTheory.cpp
@@ -1,3 +1,4 @@
+*----------------------------------------------------------------------*
 vector<int> Divisors(int n){//sqrt(n)
     vector<int> v;
     for (int i = 1; i*i <= n; i++)
@@ -10,6 +11,7 @@ vector<int> Divisors(int n){//sqrt(n)
     }
     return v;
 }
+*----------------------------------------------------------------------*
 vector<int> SumDivisorsForAllNumbers(){
     vector<ll>divsors(1e6+10,0);
     for (int i = 1; i <= (int)1e6; i++) {
@@ -18,24 +20,7 @@ vector<int> SumDivisorsForAllNumbers(){
 	}
     return divsors;
 }
-int gcd(int a,int b){ //log(n)
-    if(b>a) swap(a,b);
-    if(b==0) return a;
-    return gcd(a%b,b);
-}
-int lcm(int a,int b){
-    return a/gcd(a,b)*b;
-}
-
-bool isprime(int n){//sqrt(n)
-    if(n==1)return 0;
-    if(n!=2&&n%2==0) return 0;
-    for (int i = 3; i*i <= n; i+=2)
-    {
-        if(n%i==0)return 0;
-    }
-    return 1;
-}
+*----------------------------------------------------------------------*
 vector<int> primeFactors(int n){//sqrt(n)
     vector<int> v;
     for (int i = 2; i*i <= n; i++)
@@ -48,25 +33,27 @@ vector<int> primeFactors(int n){//sqrt(n)
     if(n>1)v.push_back(n);
     return v;
 }
-int power(int a,int b){//log(n)
-    if(b==0)return 1;
-    if(b==1)return a;
-    
-    int ret=1;
-    if(b&1) ret =  a;
-    int x = power(a,b/2);
-    return ret*x*x;
+*----------------------------------------------------------------------*
+int gcd(int a,int b){ //log(n)
+    if(b>a) swap(a,b);
+    if(b==0) return a;
+    return gcd(a%b,b);
 }
-int powermod(int a,int b,int mod){//log(n)
-    if(b==0)return 1;
-    if(b==1)return a;
-    
-    int ret=1;
-    if(b&1) ret =  a%mod;
-    int x = powermod(a,b/2,mod);
-    ret = ret*x*x;
-    return ret%mod;
+*----------------------------------------------------------------------*
+int lcm(int a,int b){
+    return a/gcd(a,b)*b;
 }
+*----------------------------------------------------------------------*
+bool isprime(int n){//sqrt(n)
+    if(n==1)return 0;
+    if(n!=2&&n%2==0) return 0;
+    for (int i = 3; i*i <= n; i+=2)
+    {
+        if(n%i==0)return 0;
+    }
+    return 1;
+}
+*----------------------------------------------------------------------*
 vector<bool> sieve(int n){//o(n)
     vector<bool> prime(n,1);
     prime[0]=prime[1]=0;
@@ -82,6 +69,7 @@ vector<bool> sieve(int n){//o(n)
     }
     return prime;
 }
+*----------------------------------------------------------------------*
 vector<bool> linearSieve(int N)//o(n)
 {
     vector<bool>isPrime(N,1);vector<int>primes;
@@ -98,6 +86,28 @@ vector<bool> linearSieve(int N)//o(n)
     }
     return isPrime;
 }
+*----------------------------------------------------------------------*
+int power(int a,int b){//log(n)
+    if(b==0)return 1;
+    if(b==1)return a;
+    
+    int ret=1;
+    if(b&1) ret =  a;
+    int x = power(a,b/2);
+    return ret*x*x;
+}
+*----------------------------------------------------------------------*
+int powermod(int a,int b,int mod){//log(n)
+    if(b==0)return 1;
+    if(b==1)return a;
+    
+    int ret=1;
+    if(b&1) ret =  a%mod;
+    int x = powermod(a,b/2,mod);
+    ret = ret*x*x;
+    return ret%mod;
+}
+*----------------------------------------------------------------------*
 int phi(int n) { //sum of coprime numbers from 1 to n //sqrt(n)
     int result = n;
     for (int i = 2; i * i <= n; i++) {
@@ -111,7 +121,7 @@ int phi(int n) { //sum of coprime numbers from 1 to n //sqrt(n)
         result -= result / n;
     return result;
 }
-
+*----------------------------------------------------------------------*
 void phi_1_to_n(int n) { //nlog(log(n))
     vector<int> phi(n + 1);
     for (int i = 0; i <= n; i++)
@@ -124,7 +134,7 @@ void phi_1_to_n(int n) { //nlog(log(n))
         }
     }
 }
-
+*----------------------------------------------------------------------*
 pair<int,int> euclid(int a,int b,int& x,int& y){  //a*x+b*y=gcd(a,b) return x,y     .........       (a*b)%m=1 and b==x if euclid(a,m);
     if(b==0){
         x=1;
@@ -138,15 +148,12 @@ pair<int,int> euclid(int a,int b,int& x,int& y){  //a*x+b*y=gcd(a,b) return x,y 
     y=x1-y1*(a/b);
     return {x,y};
 }
-
-//  mod 
+*----------------------------------------------------------------------*
 a ^ (mod - 1) % mod = 1
-add (x % mod + y % mod) % mod;
-mul (x % mod * y % mod) % mod ;
-sub (((x % mod - y % mod) % mod) + mod) % mod;
-div x / y % m
-z = powermod(y,mod-2,mod);   
-(x % mod * z % mod) % mod ;
-negative (x%m)+m%m
+int add(int x,int y,int mid) {return(x % mod + y % mod) % mod;}
+int mul(int x,int y,int mid) {return(x % mod * y % mod) % mod;}
+int sub(int x,int y,int mid) {return(((x % mod - y % mod) % mod) + mod) % mod;}
+int div(int x,int y,int mid) {return(x % mod * powermod(y,mod-2,mod) % mod) % mod;}
+int mod_negative(int x,int m) {return(x%m)+m%m;}
 
 
