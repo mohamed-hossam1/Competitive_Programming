f8da3daca2204fcee66f916bc606bf0c38d9e39d
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2024-07-17 13:49:23 +0300

    Update NumberTheory.cpp

--- NumberTheory.cpp
+++ NumberTheory.cpp
@@ -91,6 +91,32 @@ vector<bool> linearSieve(int N)//o(n)
     }
     return isPrime;
 }
+int phi(int n) { //sum of coprime numbers from 1 to n //sqrt(n)
+    int result = n;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+
+void phi_1_to_n(int n) { //nlog(log(n))
+    vector<int> phi(n + 1);
+    for (int i = 0; i <= n; i++)
+        phi[i] = i;
+
+    for (int i = 2; i <= n; i++) {
+        if (phi[i] == i) {
+            for (int j = i; j <= n; j += i)
+                phi[j] -= phi[j] / i;
+        }
+    }
+}
 
 pair<int,int> euclid(int a,int b,int& x,int& y){  //a*x+b*y=gcd(a,b) return x,y     .........       (a*b)%m=1 and b==x if euclid(a,m);
     if(b==0){
