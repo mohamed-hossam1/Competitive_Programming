0b829e4668a0cb4fe95a2bee33ed03bab69309df
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2024-07-17 20:04:17 +0300

    Update math.cpp

--- math.cpp
+++ math.cpp
@@ -1,3 +1,4 @@
+*----------------------------------------------------------------------*
          متتابعه حسابيه
       a1 a1+d a1+2d ------ an   
 int sum (int ai,int an,int n) {
@@ -9,6 +10,7 @@ int find_an(int a1,int n,int d){
 int find_d(int a1,int a2){
   return a2-a1;
 }
+*----------------------------------------------------------------------*
          متتابعه هندسيه 
       a1 a1*r a1*r*r ------ an   
 int sum (int ai,int n) {
@@ -20,16 +22,15 @@ int find_an(int a1,int n,int d){
 int find_r(int a1,int a2){
   return a2/a1;
 }
-
-
+*----------------------------------------------------------------------*
 int sum_all_toN (int n) return n = n*(n+1)/2;
 sum_allEven_toN (int n) return  n = (n/2)*((n/2)+1)/2
-
+*----------------------------------------------------------------------*
 int comp_double(double x, double y) {
         if(fabs(x-y)<=1e-9)return 0;
         return x<y?-1:1;
 }
-
+*----------------------------------------------------------------------*
 k-th_is_not_divisible_by_n(int n,int k) return k + (k - 1) / (n - 1); // n = 3 k = 7 --numbers not divisible by 3 1,2,4,5,7,8,*10*,11,13… 
 
 long long int_sqrt (long long x) {
@@ -41,4 +42,5 @@ long long int_sqrt (long long x) {
   }
   return ans;
 }
+*----------------------------------------------------------------------*
 
