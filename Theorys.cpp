96fd5a1f6b71f2defce96ef866d47c3307b4bedd
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2024-07-17 15:57:16 +0300

    Update Theorys.cpp

--- Theorys.cpp
+++ Theorys.cpp
@@ -1,2 +1,9 @@
+*----------------------------------------------------------------------*
 A number is divisible by 3 if the sum of its digits is divisible by 3
-
+*----------------------------------------------------------------------*
+the number has odd divisors if sqrt(number) == (int) sqrt(number)
+16 -> 1 2 4 8 16
+*----------------------------------------------------------------------*
+the number has three divisors if sqrt(number) == (int) sqrt(number) && sqrt(number) = prime number 
+49 -> 1 7 49
+*----------------------------------------------------------------------*
