f1153f0d34d62800bb1fb85a7669364698ac1a7d
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2024-07-19 00:22:02 +0300

    Update Theorys.cpp

--- Theorys.cpp
+++ Theorys.cpp
@@ -7,3 +7,12 @@ the number has odd divisors if sqrt(number) == (int) sqrt(number)
 the number has three divisors if sqrt(number) == (int) sqrt(number) && sqrt(number) = prime number 
 49 -> 1 7 49
 *----------------------------------------------------------------------*
+the minimum lcm for n with any number greater than n is Multiples of n 
+lcm(4,?<8)>lcm(4,8)<lcm(4,?>8)
+*----------------------------------------------------------------------*
+the range for gcd(a,b) 1->min(b,a)
+*----------------------------------------------------------------------*
+gcd(a,b) == min(b,a) when a = k*b         and == 1 if a = k*b (+|-) 1
+gcd(4,4) = gcd(4,8) = gcd(4,12) = 4       gcd(4,3) = gcd(4,5) = gcd(4,11) = 1 
+*----------------------------------------------------------------------*
+  
