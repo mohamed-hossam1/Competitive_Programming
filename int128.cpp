6f2beb6250b91ea1f419e925d222823c93d986e5
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2024-07-21 01:56:22 +0300

    Update int128.cpp

--- int128.cpp
+++ int128.cpp
@@ -20,3 +20,24 @@ void print(__int128 x) {
     putchar(x % 10 + '0');
 }
 bool cmp(__int128 x, __int128 y) { return x > y; }
+
+string multiply(string s1,string s2){
+    reverse(s1.begin(), s1.end());
+    reverse(s2.begin(), s2.end());
+    int a[600];
+    memset(a, 0, sizeof(a));
+    for (int i=0; i<s1.length(); i++) {
+        for (int j=0; j<s2.length(); j++) {
+            a[i + j] += (s1[i]-'0') * (s2[j] - '0');
+        }
+    }
+    for (int i=0; i<600 - 1; i++) {
+        a[i + 1] += a[i] / 10;
+        a[i] %= 10;
+    }
+    int i = 600 - 1;
+    while (i > 0 && a[i] == 0) i--;
+    string s="";
+    for (; i>=0; i--) s += to_string(a[i]);
+    return s;
+}
