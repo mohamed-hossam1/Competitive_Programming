f61152ce8e7634dcfa9043b520593f8e928238d1
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2024-07-28 04:32:26 +0300

    Update Combinatorics.cpp

--- Combinatorics.cpp
+++ Combinatorics.cpp
@@ -1,3 +1,14 @@
+int arr[4] = {2, 3, 5, 7};
+int n = 100;
+
+int Inclusion_Exclusion(int idx = 0, int d = 1, int sign = -1) {
+    if (idx == 4) {
+        if (d == 1)
+            return 0; 
+        return sign * n / d;
+    }
+    return Inclusion_Exclusion(idx + 1, d, sign) + Inclusion_Exclusion(idx + 1, d * arr[idx], sign * -1);
+}
 *----------------------------------------------------------------------*
 int factorial(int n) {
    if (n == 0 || n == 1){
