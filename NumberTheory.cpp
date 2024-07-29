7ecccf9de15a206b40b20e700edf01c482529716
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2024-07-29 06:31:22 +0300

    Update NumberTheory.cpp

--- NumberTheory.cpp
+++ NumberTheory.cpp
@@ -181,6 +181,32 @@ int modInverse(int a, int m)
     }
 }
 *----------------------------------------------------------------------*
+int gcd(int a, int b, int& x, int& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
+bool find_any_solution(int a, int b, int c, int &x0, int &y0, int &g) {  a*x0+b*y0=c return x0,y0
+    g = gcd(abs(a), abs(b), x0, y0);
+    if (c % g) {
+        return false;
+    }
+
+    x0 *= c / g;
+    y0 *= c / g;
+    if (a < 0) x0 = -x0;
+    if (b < 0) y0 = -y0;
+    return true;
+}
+*----------------------------------------------------------------------*
 int add(int x,int y,int mid) {return(x % mod + y % mod) % mod;}
 int mul(int x,int y,int mid) {return(x % mod * y % mod) % mod;}
 int sub(int x,int y,int mid) {return(((x % mod - y % mod) % mod) + mod) % mod;}
