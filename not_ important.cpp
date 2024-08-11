09e529b84791877356d63d798e87efdaad4e1aa0
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2024-08-11 07:12:57 +0300

    Update not_ important.cpp

--- not_ important.cpp
+++ not_ important.cpp
@@ -92,3 +92,15 @@ void allsubsequence(string str)
         }
     }
 }
+
+void findSubsets(int nums[], int n)
+{
+    for (int i = 0; i < (1 << n); i++) {
+        for (int j = 0; j < n; j++) {
+            if ((i & (1 << j)) != 0) {
+                cout << nums[j] << " ";
+            }
+        }
+        cout << endl;
+    }
+}
