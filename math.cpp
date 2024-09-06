489906fda1f714215da3a4f4760f002d7b033128
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2024-09-07 01:11:48 +0300

    Update math.cpp

--- math.cpp
+++ math.cpp
@@ -1,6 +1,4 @@
-*----------------------------------------------------------------------*
-         متتابعه حسابيه
-      a1 a1+d a1+2d ------ an   
+a1 a1+d a1+2d ------ an   
 int sum (int ai,int an,int n) {
   return ((ai + an) * n) / 2;
 }
@@ -10,9 +8,8 @@ int find_an(int a1,int n,int d){
 int find_d(int a1,int a2){
   return a2-a1;
 }
-*----------------------------------------------------------------------*
-         متتابعه هندسيه 
-      a1 a1*r a1*r*r ------ an   
+*----------------------------------------------------------------------* 
+a1 a1*r a1*r*r ------ an   
 int sum (int ai,int n) {
   return a1*(1-pow(r,n))/(1-r);
 }
@@ -25,15 +22,12 @@ int find_r(int a1,int a2){
 *----------------------------------------------------------------------*
 int sum_all_toN (int n) {return n = n*(n+1)/2;}
 int sum_allEven_toN (int n) {return  n = (n/2)*((n/2)+1);}
-double gold = (1 + sqrt(5)) / 2;
-int Fibonacci(int an){return round((pow(gold,6)-pow(1-gold,6))/sqrt(5));}  }
+double gold = (1 + sqrt(5))/2;
+int Fibonacci(int an){return round((pow(gold,6)-pow(1-gold,6))/sqrt(5));}
 *----------------------------------------------------------------------*
-int comp_double(double x, double y) {
-        if(fabs(x-y)<=1e-9)return 0;
-        return x<y?-1:1;
-}
+int comp_double(double x, double y) { if(fabs(x-y)<=1e-9)return 0; return x<y?-1:1; }
 *----------------------------------------------------------------------*
-vector<int> v = {2, 3, 5, 7};
+vector<int> v = {2,3,5,7};
 int n = 100;
 int inc_exe(int idx = 0, int d = 1, int sign = -1) {
     if (idx == v.size()) 
@@ -49,14 +43,9 @@ vector<vector<int>> multiplyMatrices(vector<vector<int>>& mat1,vector<vector<int
     int common = mat1[0].size();
     vector<vector<int>> result(rows, vector<int>(cols, 0));
 
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            for (int k = 0; k < common; ++k) {
-                result[i][j] += mat1[i][k] * mat2[k][j];
-            }
-        }
-    }
-
+    for (int i = 0; i < rows; ++i) for (int j = 0; j < cols; ++j) for (int k = 0; k < common; ++k) 
+       result[i][j] += mat1[i][k] * mat2[k][j];
+                     
     return result;
 }
 *----------------------------------------------------------------------*
@@ -83,9 +72,7 @@ pair<double, double> solveQuadratic(double a, double b, double c) { //ax^2 + bx 
 double Nth_Root_X(double x, int n) { 
     double guess = x / n;
     double epsilon = 1e-7;
-    while (abs(pow(guess, n) - x) > epsilon) {
-        guess = ((n - 1) * guess + x / pow(guess, n - 1)) / n;
-    }
+    while (abs(pow(guess, n) - x) > epsilon) guess = ((n - 1) * guess + x / pow(guess, n - 1)) / n;
     return guess;
 }
 *----------------------------------------------------------------------*
