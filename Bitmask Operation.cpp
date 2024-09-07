e460196730b9d910641e8c3807ac6b3a506deb79
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2024-09-07 18:40:55 +0300

    Update Bitmask Operation.cpp

--- Bitmask Operation.cpp
+++ Bitmask Operation.cpp
@@ -1,38 +1,39 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define ll long long
-bool Knowbit(ll n,int i)
-{
-    return (n>>i)&1;
-}
-ll Setbit(ll n,int i)
-{
-    return n|(1<<i);
-}
-ll Resetbit(ll n,int i)
-{
-    return n&(~(1<<i));
-}
-ll flip(ll n,int i)
-{
-    return n^(1<<i);
-}
-bool isPowerOfTwo(int n)
-{
-    if(n==0)return 0;
-    return !(n&(n-1));
-}
-int countFoOnes(int n){
-    return __builtin_popcount(n);
-}
  and       or      xor 
 1 1 1    0 1 1    0 1 1
 1 0 0    0 0 0    0 0 0
 0 1 0    1 1 1    1 1 0
 0 0 0    1 0 1    1 0 1 
-
-a^b = c
-c^a = b
+bool Knowbit(ll n,int i){ return (n >> i) & 1; }
+ll Setbit(ll n,int i){return n | (1 << i); }
+ll Resetbit(ll n,int i){return n & ( ~ (1 << i)); }
+ll flip(ll n,int i){return n ^ (1 << i); }
+bool isPowerOfTwo(int n){if(n==0)return 0; return !(n & (n - 1)); }
+__builtin_popcount(n); //count the number of one’s
+__builtin_ctz(x) //zeros from last to first occurrence of one
+__builtin_clz(x) //number of zeros before the first occurrence of one
 bitset<32> a;
 a.count(); // count of ones
-// find all subsets for any array by size ~ 20
+a.to_ullong() //to int
+*----------------------------------------------------------------------*
+void Subsets(vector<int> v) // find all subsets for any array by size ~ 20
+{
+    for (int i = 0; i < (1<<v.size()); i++){   
+        int k=i;
+        while (k)
+        {
+            if(k%2!=0){
+                cout<<v[i]<<" ";
+            }
+            j++,k/=2;
+        }
+        cout<<endl;
+    }
+}
+*----------------------------------------------------------------------*
+Theorys
+a^b = c
+c^a = b
+a+b = (a^b)+2(a&b)
+a^b^a=b
+    
+
