26a33698b7a73ba6c8262abd27b8e3dfab085d70
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2024-09-11 04:09:48 +0300

    Update Combinatorics.cpp

--- Combinatorics.cpp
+++ Combinatorics.cpp
@@ -1,7 +1,9 @@
-int fact[N];
+const int N = 1e6+7,MOD = 1e9 + 7;
+int fact[N],modinv[N];
+ 
 int fastpow(int a,int b) {
     int result = 1;
-    while (b > 0) {
+    while (b) {
         if (b % 2 == 1)
             result = (result * a) % MOD;
         a = (a * a) % MOD;
@@ -9,20 +11,22 @@ int fastpow(int a,int b) {
     }
     return result;
 }
-
+ 
 void factorial() {
     fact[0] = 1;
     for (int i = 1; i < N; i++) fact[i] = i*fact[i-1] % MOD;
+    modinv[N - 1] = fastpow(fact[N - 1], MOD - 2);
+    for (int i = N - 2; i >= 0; i--)
+        modinv[i] = (i + 1) * modinv[i + 1] % MOD;
 }
-
-int modInverse(int a) {return fastpow(a,MOD-2);}
-
+  
 int nCr(int n, int r) {
     return (fact[n] * modInverse(fact[r]) % MOD * modInverse(fact[n - r]) % MOD) % MOD;
+ 
 }
-
+ 
 int nPr(int n, int r) {
-    return (fact[n] * modInverse(fact[n - r])% mod) % mod;
+    return (fact[n] * modInverse(fact[n - r])% MOD) % MOD;
 }
 *----------------------------------------------------------------------* 
 int npr(int n,int r){
