5229fb7146d43aa2b82e627943006ab036c2834d
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2024-09-11 22:06:34 +0300

    Update Geometry2.cpp

--- Geometry2.cpp
+++ Geometry2.cpp
@@ -1,1 +1,13 @@
+#define PI acos(-1.0)
+#define df(b, a) cout << fixed << setprecision(b) << a << endl; 
+#define point complex<double>
+vector
+double dot(point a,point b){return (conj(a)*b).real();}
+double cross(point a,point b){return (conj(a)*b).imag();}
+double distance(point a) {return abs(a);}
+double angle(point a,point b){return atan2(cross(a,b),dot(a,b));}//a,b!=b,a 
+double angle_withX(point a){return atan2(a.real(),a.imag());}
+point rotate(point p, double angle) {return polar(1.0,angle)*p;}
+point reflect(point a, point b) {return (a-b)*(polar(1.0,PI))+(b);}
+
 
