a798488445782b98b17aa78db89577771e1b9a5a
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2024-09-27 04:09:10 +0300

    Update NumberTheory.cpp

--- NumberTheory.cpp
+++ NumberTheory.cpp
@@ -252,7 +252,7 @@ pair<int, int> Fibonacci (int n) { return fn,fn+1
     if (n == 0)
         return {0, 1};
 
-    auto p = fib(n >> 1);
+    auto p = Fibonacci(n >> 1);
     int c = p.first * (2 * p.second - p.first);
     int d = p.first * p.first + p.second * p.second;
     if (n & 1)
