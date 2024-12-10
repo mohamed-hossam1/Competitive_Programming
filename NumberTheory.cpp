2b87febb0a16ca54643c8e568b607b280c84fbbf
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2024-12-10 22:35:30 +0200

    Update NumberTheory.cpp

--- NumberTheory.cpp
+++ NumberTheory.cpp
@@ -6,7 +6,6 @@ int mul(int x,int y,int mid) {return(x % mod * y % mod) % mod;}
 int sub(int x,int y,int mid) {return(((x % mod - y % mod) % mod) + mod) % mod;}
 int div(int x,int y,int mid) {return(x % mod * powermod(y,mod-2,mod) % mod) % mod;}
 int mod_negative(int x,int m) {return(x%m)+m%m;}
-*----------------------------------------------------------------------*
 int mul(int x, int y, int m) {
     if (x < y) return productm(y, x, m);
     if (y == 0) return 0;
