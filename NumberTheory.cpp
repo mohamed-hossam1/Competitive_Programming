428157a7017b22f3b643f255d7a8d0fc515d5832
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2025-04-03 04:14:38 +0200

    Update NumberTheory.cpp

--- NumberTheory.cpp
+++ NumberTheory.cpp
@@ -176,6 +176,17 @@ int power(int N,int M) {
     return half;
 }
 *----------------------------------------------------------------------*
+int binpow(int a, int b) {
+    int res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+*----------------------------------------------------------------------*
 int power(int N,int M,int MOD) {
     if (M == 0) return 1;
     
